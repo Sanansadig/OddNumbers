a9b41cc99407286e3fd9ce18d6877926ddd426d7
Sanansadig <sanan.sadig@hotmail.com>
2017-05-04 19:02:40 +0400

    OddNumbers

---
+++ oddNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oddNumbers
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int cavab = math(6);
+            Console.WriteLine(cavab);
+            Console.ReadLine();
+        }
+
+        static int math(int a)
+        {
+            if (a%2==0)
+            {
+                return a * a;
+            }
+            else
+            {
+                return a * a * a;
+            }
+        }
+    }
+}
